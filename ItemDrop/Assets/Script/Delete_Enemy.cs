5ee55ac9f591e786e48a14b14cf850d2a736d618
ryousuke <83821881+Ryosuke004682@users.noreply.github.com>
2022-11-15 15:08:24 +0900

    敵を倒したらマグロをドロップできた。

--- ItemDrop/Assets/Script/Delete_Enemy.cs
+++ ItemDrop/Assets/Script/Delete_Enemy.cs
@@ -6,27 +6,31 @@ public class Delete_Enemy : MonoBehaviour
 {
     public GameObject obj;
 
+    Rigidbody _rb;
+
     [SerializeField]
     public ItemDrop item;
     
 
     private void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
+        _rb.freezeRotation = true;
+        _rb.constraints = RigidbodyConstraints.FreezePosition;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Sword")
+        if (other.gameObject.tag == "Sword")
         {
-            Destroy(gameObject);
-        }
+            Destroy(this.gameObject);
 
-        if(item.ItemNaming() == "マグロ")
-        {
-            Instantiate(obj,transform.position , Quaternion.identity);
+            if (item.ItemNaming() == "マグロ")
+            {
+                Instantiate(obj, transform.position, Quaternion.identity);
 
-            Debug.Log($"{item.ItemNaming()}が落ちた！");
+                Debug.Log($"{item.ItemNaming()}が落ちた！");
+            }
         }
     }
 }
